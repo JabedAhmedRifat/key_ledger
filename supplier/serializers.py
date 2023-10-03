1a54f55fb39536bdf4871f1f169f2f0130d37d8f
Md. Jabed Ahmed <jabedahmedrifat1@gmail.com>
2023-10-03 13:17:33 +0600

    supplier order total in words

--- supplier/serializers.py
+++ supplier/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import *
+from num2words import num2words
+
 
 class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,6 +15,9 @@ class SupplierSerializer(serializers.ModelSerializer):
 #         fields = "__all__"
         
 class SupplierOrderSerializer(serializers.ModelSerializer):
+    
+    order_total_words = serializers.SerializerMethodField()
+    
     class Meta:
         model = SupplierOrder
         fields = '__all__'
@@ -23,6 +28,14 @@ class SupplierOrderSerializer(serializers.ModelSerializer):
         instance.order_number = f'DSA{instance.id}'
         instance.save()
         return instance
+    
+    
+    def get_order_total_words(self,data):
+        order_total = data.order_total
+        if order_total is not None:
+            return num2words(order_total, to='cardinal')
+        else: 
+            return ""
     
     
     
