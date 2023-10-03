e00eb9c1c6a816cdc706576a3261ba4497926897
Md. Jabed Ahmed <jabedahmedrifat1@gmail.com>
2023-10-03 13:35:32 +0600

    customer order_total in word done

--- product/serializers.py
+++ product/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import *
 
+from num2words import num2words
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -10,6 +12,8 @@ class ProductSerializer(serializers.ModelSerializer):
 # customer Order an and Order product Serializer
     
 class OrderSerializer(serializers.ModelSerializer):
+    order_total_word = serializers.SerializerMethodField()
+    
     class Meta:
         model = Order
         fields = '__all__'
@@ -20,6 +24,13 @@ class OrderSerializer(serializers.ModelSerializer):
         instance.order_number = f'CUS{instance.id}'
         instance.save()
         return instance
+    
+    def get_order_total_word(self, data):
+        order_total = data.order_total
+        if order_total is not None:
+            return num2words(order_total)
+        else:
+            return ""
     
         
 class orderProductSerializer(serializers.ModelSerializer):
